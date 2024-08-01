6f9899bef1991f185c0a38d12b4172355a5b1f35
Hugo Moreira <hugo.cesar91@gmail.com>
2024-07-31 23:45:52 -0300

    Responsivo e validação do formulário finalizados

---
+++ contact-form-main/assets/js/form-validation.js
@@ -0,0 +1,79 @@
+const form = document.getElementById('form-contact');
+const inputsText = document.querySelectorAll('input[type="text"]');
+const inputEmail = document.querySelector('input[type="email"]');
+const inputsRadio = document.querySelectorAll('input[type="radio"]');
+const inputTextArea = document.querySelector('textarea');
+const inputCheckbox = document.querySelector('input[type="checkbox"]');
+
+console.log(inputsRadio);
+
+const requireMessage = document.querySelectorAll('.require-msg');
+const requireMessageTxtArea = document.querySelector('.require-msg-textarea');
+const validMessage = document.querySelector('.valid-msg');
+const queryMessage = document.querySelector('.query-msg');
+const consentMessage = document.querySelector('.consent-msg');
+
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+form.addEventListener('submit', (event)=>{
+    let validForm = false;
+
+    inputsText.forEach(function(input, index){
+        if(input.value === ""){
+            requireMessage[index].classList.add('visible');
+            input.classList.add('error');
+            validForm = false;
+        }else{
+            requireMessage[index].classList.remove('visible');
+            input.classList.remove('error');
+            validForm = true;
+        }
+    });
+
+    if(inputEmail.value === ""){
+        requireMessage[2].classList.add('visible');
+        inputEmail.classList.add('error');
+        validForm = false;
+    }else if(!emailRegex.test(inputEmail.value)){
+        validMessage.classList.add('visible');
+        inputEmail.classList.add('error');
+        validForm = false;
+    }else{
+        requireMessage[2].classList.remove('visible');
+        validMessage.classList.remove('visible');
+        inputEmail.classList.remove('error');
+        validForm = true;
+    }
+
+    inputsRadio.forEach(radioButton =>{
+        if(radioButton.checked){
+            queryMessage.classList.remove('visible');
+            validForm = true;
+        }else{
+            queryMessage.classList.add('visible');
+            validForm = false;
+        }
+    });
+
+    if(inputTextArea.value === ""){
+        requireMessageTxtArea.classList.add('visible');
+        inputTextArea.classList.add('error');
+        validForm = false;
+    }else{
+        requireMessageTxtArea.classList.remove('visible');
+        inputTextArea.classList.remove('error');
+        validForm = true;
+    }
+
+    if(inputCheckbox.checked){
+        consentMessage.classList.remove('visible');
+        validForm = true;
+    }else{
+        consentMessage.classList.add('visible');
+        validForm = false;
+    }
+
+    if(!validForm){
+        event.preventDefault();
+    }
+});
